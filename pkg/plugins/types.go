b1c100abadedf7343a2f3b20817a3d8a8bb3aed4
Thomas Hendrickson <thomas.hendrickson@praetorian.com>
2023-01-17 16:40:53 -0500

    ssh password auth

--- pkg/plugins/types.go
+++ pkg/plugins/types.go
@@ -427,11 +427,12 @@ type ServiceStun struct {
 func (e ServiceStun) Type() string { return ProtoStun }
 
 type ServiceSSH struct {
-	Banner             string `json:"banner"`
-	Algo               string `json:"algo"`
-	HostKey            string `json:"hostKey,omitempty"`
-	HostKeyType        string `json:"hostKeyType,omitempty"`
-	HostKeyFingerprint string `json:"hostKeyFingerprint,omitempty"`
+	Banner              string `json:"banner"`
+	PasswordAuthEnabled bool   `json:"passwordAuthEnabled"`
+	Algo                string `json:"algo"`
+	HostKey             string `json:"hostKey,omitempty"`
+	HostKeyType         string `json:"hostKeyType,omitempty"`
+	HostKeyFingerprint  string `json:"hostKeyFingerprint,omitempty"`
 }
 
 func (e ServiceSSH) Type() string { return ProtoSSH }
